6b0ea72eff48b92f543deb0ce4e996c6686ee988
Vale Restan <valeresmune10@gmail.com>
2025-02-12 13:41:14 -0500

    avance

---
+++ bit-spa/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'bit-spa';
+}
