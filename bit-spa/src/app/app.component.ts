7a5a7cf5c27c68407fdc8a491b69c4502c685920
Vale Restan <valeresmune10@gmail.com>
2025-02-12 21:56:06 -0500

    avance

--- bit-spa/src/app/app.component.ts
+++ bit-spa/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { NavComponent } from './component/nav/nav.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, NavComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- bit-spa/src/app/app.routes.ts
+++ bit-spa/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { from } from 'rxjs';
+import { HomeComponent } from './component/home/home.component';
+import { FoundationsComponent } from './component/foundations/foundations.component';
+import { FooterComponent } from './component/footer/footer.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'inicio', component: HomeComponent },
+  { path: 'fundamentos', component: FoundationsComponent },
+  { path: 'contacto', component: FooterComponent },
+];
