0dfb8f586f3bc29902468ba3d115afef1cab8671
Vale Restan <valeresmune10@gmail.com>
2025-02-13 19:16:45 -0500

    avance

--- bit-spa/src/app/app.component.ts
+++ bit-spa/src/app/app.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { NavComponent } from './component/nav/nav.component';
+import { ContactoComponent } from './component/contacto/contacto.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, NavComponent, RouterOutlet],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ContactoComponent,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- bit-spa/src/app/app.routes.ts
+++ bit-spa/src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import { from } from 'rxjs';
 import { HomeComponent } from './component/home/home.component';
 import { FoundationsComponent } from './component/foundations/foundations.component';
-import { FooterComponent } from './component/footer/footer.component';
+import { ContactoComponent  } from './component/contacto/contacto.component';
 
 
 export const routes: Routes = [
   { path: 'inicio', component: HomeComponent },
   { path: 'fundamentos', component: FoundationsComponent },
-  { path: 'contacto', component: FooterComponent },
+  { path: 'contacto', component: ContactoComponent },
 ];
