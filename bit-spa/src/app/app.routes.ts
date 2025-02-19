10349b4e77e56f224716c5cb11e19eef3a19f043
Vale Restan <valeresmune10@gmail.com>
2025-02-18 21:29:42 -0500

    valen

--- bit-spa/src/app/app.routes.ts
+++ bit-spa/src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { from } from 'rxjs';
 import { HomeComponent } from './component/home/home.component';
 import { FoundationsComponent } from './component/foundations/foundations.component';
 import { ContactoComponent  } from './component/contacto/contacto.component';
-
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import {ApiComponent} from './component/api/api.component';
+import {PokeapiComponent} from './component/pokeapi/pokeapi.component';
 
 export const routes: Routes = [
-  { path: 'inicio', component: HomeComponent },
-  { path: 'fundamentos', component: FoundationsComponent },
-  { path: 'contacto', component: ContactoComponent },
+  { path: 'api', component: ApiComponent, title: 'Api' },
+  { path: 'contacto', component: ContactoComponent, title: 'Contacto' },
+  { path: 'inicio', component: HomeComponent, title: 'Inicio' },
+  { path: 'tendecias', component: FoundationsComponent, title: 'Tendencias' },
+  {path: 'pokeapi', component: PokeapiComponent, title: "Pokeapi"}, 
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent, title: 'Error 404' },
 ];
--- bit-spa/src/app/component/foundations/foundations.component.ts
+++ bit-spa/src/app/component/foundations/foundations.component.ts
@@ -7,27 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './foundations.component.css',
 })
 export class FoundationsComponent {
-  nombreUsuario: string = 'pepita';
-  estalogueado: boolean = true;
-  devs: string[] = ['Juan', 'Pedro', 'Maria'];
-
-  ooss = [
-    { id: 'win', name: 'Windows' },
-    { id: 'osx', name: 'MacOS' },
-    { id: 'linux', name: 'Linux' },
-  ];
-
-  evitarenvio: boolean = false;
-  urlimg: string =
-    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fethic.es%2F2023%2F03%2Fel-enigma-de-la-imagen%2F&psig=AOvVaw0yhqMrIGmzQ_hmE1jhDH__&ust=1739585836573000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLjz67CMwosDFQAAAAAdAAAAABAE';
-
-  manejarclick() {
-    console.log('hiciste click');
-  }
-
-  colorfondo: string = 'cf-rojo';
-  manejarmouseover(){
-    this.colorfondo = "cf-amarillo"
-  }
+  
 }
 
